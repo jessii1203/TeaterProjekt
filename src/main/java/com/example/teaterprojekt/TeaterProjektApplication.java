edf9b32a2bbd434b46d1507d6b0417c0dcc52d9b
jessi1203 <jessii.ca@live.dk>
2023-03-15 10:29:13 +0100

    Initial commit

---
+++ src/main/java/com/example/teaterprojekt/TeaterProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.teaterprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaterProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeaterProjektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/teaterprojekt/TeaterProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teaterprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaterProjektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
